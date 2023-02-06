e5420cd8c9bcf1b3910de856a84fbffbb204ca62
umsharma-ext_afklm <umsharma-ext@airfrance.fr>
2023-02-06 20:59:57 +0100

    add script

---
+++ scripts/chef.sh
@@ -0,0 +1,38 @@
+# export the values as environment variables
+# export AZURE_SUBSCRIPTION_ID=$INPUT_SUBSCRIPTIONID
+# export AZURE_CLIENT_ID=$INPUT_CLIENTID
+# export AZURE_TENANT_ID=$INPUT_TENANTID
+# export AZURE_CLIENT_SECRET=$INPUT_CLIENTSECRET
+export KEYVAULT_NAME=$KEYVAULT
+# export KEYVAULT=$KEYVAULT
+echo "test output of environment variables"
+echo "---------------------------------"
+echo "Keyvault name is $KEYVAULT_NAME"
+echo "Keyvault name is $KEYVAULT"
+echo "---------------------------------"
+# exit 0
+
+# # install the Chef Inspec
+# curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P inspec 
+
+# test install specific version
+wget -q https://packages.chef.io/files/stable/inspec/5.21.29/ubuntu/20.04/inspec_5.21.29-1_amd64.deb
+echo 35ed1da6c885edd1618c19eb0305eaf2836cfae33b21ebd327d31512f15140d7 inspec_5.21.29-1_amd64.deb|sha256sum -c  
+sudo dpkg -i inspec_5.21.29-1_amd64.deb
+echo "---------------------------------"
+
+# execute Azure tests
+# inspec exec inspec/test/ --input=URL="kv-test-weu-50e2b310.vault.azure.net" KEYVAULT=$KEYVAULT -t azure:// --chef-license accept-silent --reporter cli html:azure_test.html 
+
+
+cat inspec/test/inputs.yaml
+envsubst < inspec/test/inputs.yaml
+(cat inspec/test/inputs.yaml | envsubst) >> inspec/test/inputs-sub.yaml
+
+TEST_VAR="test"
+TEST_DIRECTORY="inspec/${TEST_VAR}"
+
+inspec exec ${TEST_DIRECTORY} --input-file ${TEST_DIRECTORY}/inputs-sub.yaml -t azure:// --chef-license accept-silent --reporter cli html:azure_test.html
+
+# execute keyvault test
+# inspec exec inspec/access/ --input=URL="kv-test-weu-50e2b310.vault.azure.net" KEYVAULT=$KEYVAULT --chef-license accept-silent --reporter cli html:keyvault_test.html 
---
+++ scripts/set-workflow-env-variables.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# source env file and set values as environment variables
+while read -r line
+do
+    echo "$line" >> $GITHUB_ENV
+    export $line
+done < ./conf/$1.env
+
+# determine TF_VAR_location
+case $2 in
+    weu)
+    echo "TF_VAR_location=westeurope" >> $GITHUB_ENV
+    ;;
+    frc)
+    echo "TF_VAR_location=francecentral" >> $GITHUB_ENV
+    ;;
+esac
+
+case $3 in
+    false)
+    echo "TF_VAR_new_deployment1=false" >> $GITHUB_ENV
+    ;;
+    true)
+    echo "TF_VAR_new_deployment1=true" >> $GITHUB_ENV
+    ;;
+esac
+
+#  echo "TF_VAR_shared_key=${VPN_SECRET}" >> $GITHUB_ENV
+
+ echo "TF_VAR_testput1=$VPN_SECRET" >> $GITHUB_ENV
+ echo "TF_VAR_testput2=${VPN_SECRET}" >> $GITHUB_ENV
+ echo "TF_VAR_testput3="${VPN_SECRET}"" >> $GITHUB_ENV
+
+echo "TF_CLI_ARGS_plan=$(echo $TF_CLI_ARGS_plan | envsubst)" >> $GITHUB_ENV
+echo "TF_CLI_ARGS_apply=$(echo $TF_CLI_ARGS_apply | envsubst)" >> $GITHUB_ENV
+echo "TF_CLI_ARGS_destroy=$(echo $TF_CLI_ARGS_destroy | envsubst)" >> $GITHUB_ENV
