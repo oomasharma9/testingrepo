edb8da39b34ac387597efac73df82601e0250cdd
umsharma-ext_afklm <umsharma-ext@airfrance.fr>
2023-02-07 13:39:33 +0100

    updating providers

--- branch-test/providers.tf
+++ branch-test/providers.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    # resource_group_name  = "tfstatetestfakkel"
-    # storage_account_name = "tfstatetestfakkel"
-    container_name = "tfstate"
+    resource_group_name  = "RG1"
+    storage_account_name = "testingreposa1"
+    container_name       = "tfstate"
   }
   required_providers {
     azurerm = {
