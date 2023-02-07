03d36f5cfed2812ea8d319c497d6417248307f84
umsharma-ext_afklm <umsharma-ext@airfrance.fr>
2023-02-07 15:41:10 +0100

    add AG code

---
+++ action-group-test/action-group.tf
@@ -0,0 +1,56 @@
+locals {
+  email_receiver_group      = var.location_code == "frc" ? local.single_email : local.multiple_emails
+  email_receiver_group-test = var.location_code == "frc" ? var.single_email : var.multiple_emails
+
+  single_email = [
+    { name = "single", email_address = "single@test.nl", use_common_alert_schema = true }
+  ]
+
+  multiple_emails = [
+    { name = "test1", email_address = "123@test.nl", use_common_alert_schema = true },
+    { name = "test2", email_address = "456@test.nl", use_common_alert_schema = true }
+  ]
+  local1 = "peanut"
+}
+
+
+resource "azurerm_resource_group" "test-group-rg" {
+  name     = "test-group-rg-resources"
+  location = var.resource_group_location
+}
+
+resource "azurerm_resource_group" "action-group-rg" {
+  name     = "action-group-rg-resources"
+  location = var.resource_group_location
+}
+
+resource "azurerm_monitor_action_group" "test-group" {
+  count               = var.location_code == "weu" && local.local1 != "peanut" ? 1 : 0
+  name                = "action-test-after-name-change"
+  resource_group_name = azurerm_resource_group.action-group-rg.name
+  short_name          = "testgroup1"
+}
+
+resource "azurerm_monitor_action_group" "action-group" {
+  name                = "action-group-rg-testcase"
+  resource_group_name = azurerm_resource_group.action-group-rg.name
+  short_name          = "shortername"
+
+  dynamic "arm_role_receiver" {
+    for_each = var.location_code == "frc" ? [] : [1]
+    content {
+      name                    = "AzureAdvisorAlerts${var.location_code}"
+      role_id                 = var.role-id-owner
+      use_common_alert_schema = true
+    }
+  }
+
+  dynamic "email_receiver" {
+    for_each = local.email_receiver_group-test
+    content {
+      name                    = email_receiver.value.name
+      email_address           = email_receiver.value.email_address
+      use_common_alert_schema = true
+    }
+  }
+}
---
+++ action-group-test/providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "azurerm" {
+    # resource_group_name  = "RG1"
+    # storage_account_name = "testingreposa1"
+    container_name = "tfstate"
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.40.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
