399d6b4f24f483ee04f3d10872e306b95c4bf80c
umsharma-ext_afklm <umsharma-ext@airfrance.fr>
2023-05-03 14:28:23 +0200

    index

--- action-group-test/alerts.tf
+++ action-group-test/alerts.tf
@@ -2,7 +2,7 @@ resource "azurerm_monitor_metric_alert" "windows_cpu_percentage" {
   name                = "alert-cpupercentage"
   resource_group_name = azurerm_resource_group.action-group-rg.name
   severity            = 0
-  scopes              = [azurerm_storage_account.test.id]
+  scopes              = [azurerm_storage_account.test[count.index].id]
   description         = "Action will be triggered when Transactions count is greater than 85."
   criteria {
     metric_namespace = "Microsoft.Storage/storageAccounts"
