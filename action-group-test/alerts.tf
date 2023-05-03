71eb6ea2a09c939c622d18d992e989be496f993d
umsharma-ext_afklm <umsharma-ext@airfrance.fr>
2023-05-03 14:10:02 +0200

    ag rg string

--- action-group-test/alerts.tf
+++ action-group-test/alerts.tf
@@ -2,7 +2,7 @@ resource "azurerm_monitor_metric_alert" "windows_cpu_percentage" {
   name                = "alert-cpupercentage"
   resource_group_name = azurerm_resource_group.action-group-rg.name
   severity            = 0
-  scopes              = azurerm_resource_group.action_group_id.id
+  scopes              = [azurerm_resource_group.action_group_id.id]
   description         = "Action will be triggered when Transactions count is greater than 85."
   criteria {
     metric_namespace = "microsoft.resources/subscriptions"
